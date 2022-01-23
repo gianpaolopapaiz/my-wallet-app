858d6de78e41c2d7edfe08e81f313b751014ce9e
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-01-23 17:12:00 -0300

    Add pundit for account model

---
+++ app/policies/account_policy.rb
@@ -0,0 +1,18 @@
+class AccountPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+end
