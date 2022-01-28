e4643fdd3453dbee5a988e5b14fb420a69aeefa1
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-01-27 22:11:23 -0300

    Add index and edit account methods and view

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,10 @@
 class AccountsController < ApplicationController
   def index
+    @accounts = policy_scope(Account).order(created_at: :desc)
+  end
 
+  def new
+    @account = Account.new
+    authorize @account
   end
 end
