7bdb80e07268204d7b1be1517d5fa88fe4f5f640
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-01-31 23:11:56 -0300

    Add category name and description validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
   # Associations
   belongs_to :user
   # Validations
-  validates :name, presence: true
+  validates :name, length: { maximum: 20 }, presence: true
+  validates :description, length: { maximum: 50 }
 end
