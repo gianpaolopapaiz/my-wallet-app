6c78e6b12edce69a70d2ad9512b6e5ad7b8e5a3b
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-01-31 23:39:21 -0300

    Add transactions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'accounts#index'
-  resources :accounts
+
+  resources :accounts do
+    resources :transactions, only: [:index, :new, :create]
+  end
+
+  resources :transactions, only: [:edit, :update, :destroy]
+
   resources :categories
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
