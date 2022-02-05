f249d3d05ef97778c69ea8b18f41b0106e1873b9
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-02-05 20:30:23 -0300

    Add has many transactions association to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   # Associations
   belongs_to :user
+  has_many :transactions
   # Validations
   validates :name, length: { maximum: 20 }, presence: true
   validates :description, length: { maximum: 50 }
