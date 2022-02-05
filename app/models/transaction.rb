e3d1d19d05b39d4a30e7286d422c60c323552736
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2022-02-05 20:29:57 -0300

    Alter name max length and add category to optional true on transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction < ApplicationRecord
   # Associations
-  belongs_to :category
+  belongs_to :category, optional: true
   belongs_to :account
   # Validations
   validates :date, presence: true
-  validates :name, length: { maximum: 20 }, presence: true
+  validates :name, length: { maximum: 30 }, presence: true
   validates :description, length: { maximum: 50 }
   validates :value, numericality: true
 
